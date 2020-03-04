57cd7766606e5d56dd36c7836bd2300a5149eac5
Alexander <alexander@pop-os.localdomain>
2020-03-04 23:08:48 +0100

    Major overhaul. Dynamic and theor. infinite now.

--- 24_planet_of_discord/main.py
+++ 24_planet_of_discord/main.py
@@ -3,14 +3,15 @@ Solution to Day 24 of the Advent of Code 2019.
 Title: 'Planet of Discord'
 URL: https://adventofcode.com/2019/day/24
 Author: tmpr
-Date: 23th of February
+Date: 4th of March
 """
 
 import os
 
 from bug_field import BugField
-from recursive_bug_field import RecursiveBugField
 from functions import calculate_biodiversity
+from recursive_bug_field import RecursiveBugField
+
 
 def main():
     with open(os.path.join("inputs", "my_input.in"), "r") as f:
@@ -18,7 +19,7 @@ def main():
     
     my_bug_field = BugField(my_input_string)
     
-    rec_bug_field = RecursiveBugField(my_input_string, max_recursion_depth=310)
+    rec_bug_field = RecursiveBugField(my_input_string)
     rec_bug_field.forward(minutes = 200)
 
     print("Answer 1: ")
--- 24_planet_of_discord/recursive_bug_field.py
+++ 24_planet_of_discord/recursive_bug_field.py
@@ -1,12 +1,10 @@
 """File containing only the RecursiveBugField class."""
 
 import numpy as np
-import os
 
-from functions import point
-from constants import COORD_NEXT_TO_MIDDLE, DEAD, ALIVE
-from constants import OPPOSITE, EMPTY_FIELD
 from bug_field import BugField
+from constants import ALIVE, COORD_NEXT_TO_MIDDLE, DEAD, EMPTY_FIELD, OPPOSITE
+from functions import point
 
 
 class RecursiveBugField(BugField):
@@ -15,61 +13,25 @@ class RecursiveBugField(BugField):
     another RecursiveBugField as its middle cell, while being
     the middle cell of another RecursiveBugField, i.e. the RBF
     has a super- and a subfield.
-
-    Due to limitations of Python, Infinity cannot be modelled,
-    thus specify a `max_recursion_depth`, which determines how
-    many 'layers' the field has in each direction. 
     """
 
-    def __init__(self, field_as_string: str, depth=0, sub_field=None,
-                 super_field=None, max_recursion_depth=50):
+    def __init__(self, field_as_string: str, sub_field=None, super_field=None):
         self.state = self.get_initial_state(field_as_string)
-        self.depth = depth
-        self.max_recursion = max_recursion_depth
 
-        self.create_other_field(sub_field, "sub")
-        self.create_other_field(super_field, "super")
+        self.super_field = super_field
+        self.sub_field = sub_field
 
-        if self.sub_field:
-            self.state[2, 2] = self.sub_field
-
-    def create_other_field(self, other_field, mode):
-        """
-        If allowed by the max recursion depth, creates super- or subfield.
-
-        - Modes: 
-        `'super'` - Creates superfield. Needs passed param `super_field`.\n
-        `'sub'` - Creates subfield. Needs passed param `sub_field.`\n
-        """
-        if mode == "sub":
-            if self.depth == - self.max_recursion:
-                self.sub_field = None
-            elif other_field:
-                self.sub_field = other_field
-            else:
-                self.sub_field = RecursiveBugField(EMPTY_FIELD,
-                                    depth=self.depth - 1,
-                                    super_field=self,
-                                    max_recursion_depth=self.max_recursion)
-
-        elif mode == "super":
-            if self.depth == self.max_recursion:
-                self.super_field = None
-            elif other_field:
-                self.super_field = other_field
-            else:
-                self.super_field = RecursiveBugField(EMPTY_FIELD,
-                                        depth=self.depth + 1,
-                                        sub_field=self,
-                                        max_recursion_depth=self.max_recursion)
-        else:
-            raise ValueError(f"Unknown mode: {mode}")
+        self.state[2, 2] = "?"
 
     def forward(self, minutes=1):
         """Advances entire recursive field given amount of minutes."""
         for _ in range(minutes):
             highest_field = self.get_highest_field()
             highest_field.compute_next_states()
+
+            # When computing next states, 
+            # the highest field might change.
+            highest_field = self.get_highest_field()
             highest_field.update_states()
 
     def get_highest_field(self):
@@ -83,7 +45,7 @@ class RecursiveBugField(BugField):
         """Computes next state for all fields."""
         if self.sub_field:
             self.sub_field.compute_next_states()
-        self.next_state = self.get_next_state()
+        self.set_next_state()
 
     def update_states(self):
         """Assigns the value of the next state to the current state."""
@@ -91,52 +53,39 @@ class RecursiveBugField(BugField):
             self.sub_field.update_states()
         self.state = self.next_state
 
-    def get_next_state(self):
-        """
-        Applies rules for own state of the field and 
-        thenceforth returns that computed next state.
-        """
-        next_state = np.array(self.state)
+    def set_next_state(self):
+        """Sets next state according to rules."""
+        self.next_state = np.array(self.state)
 
-        y_length, x_length = self.state.shape
-        for y_coord in range(y_length):
-            for x_coord in range(x_length):
-                coord = (y_coord, x_coord)
+        for coord in np.argwhere(self.state != False):
+            coord = tuple(coord)
+            if coord == (2, 2): 
+                continue
 
-                if coord == (2, 2):
-                    continue
+            adj_bugs = sum([self.alive_neighbors("right_of", coord),
+                            self.alive_neighbors("left_of", coord),
+                            self.alive_neighbors("below", coord),
+                            self.alive_neighbors("above", coord)])
 
-                adj_bugs = 0
-
-                adj_bugs += self.alive_neighbors("right_of", coord)
-                adj_bugs += self.alive_neighbors("left_of", coord)
-                adj_bugs += self.alive_neighbors("below", coord)
-                adj_bugs += self.alive_neighbors("above", coord)
-
-                if self.state[coord] == ALIVE:
-                    if adj_bugs == 1:
-                        if abs(self.depth) == self.max_recursion:
-                            raise Warning("Altered outermost level.")
-                        next_state[coord] = ALIVE
-                    else:
-                        next_state[coord] = DEAD
+            if self.state[coord] == ALIVE:
+                if adj_bugs == 1:
+                    self.next_state[coord] = ALIVE
                 else:
-                    if adj_bugs in [1, 2]:
-                        if abs(self.depth) == self.max_recursion:
-                            raise Warning("Altered outermost level.")
-                        next_state[coord] = ALIVE
-                    else:
-                        next_state[coord] = DEAD
-
-        return next_state
+                    self.next_state[coord] = DEAD
+            else:
+                if adj_bugs in [1, 2]:
+                    self.next_state[coord] = ALIVE
+                else:
+                    self.next_state[coord] = DEAD
 
     def total_bugs(self):
-        """Returns amount of entire recursive field."""
+        """Returns amount of bugs of entire recursive field."""
         highest_field = self.get_highest_field()
         total_bugs = highest_field.number_of_bugs()
         return total_bugs
 
     def number_of_bugs(self):
+        """Returns number of bugs of single bugfield."""
         if self.sub_field:
             bugs_below = self.sub_field.number_of_bugs()
         else:
@@ -150,22 +99,34 @@ class RecursiveBugField(BugField):
         """
         alive_neighbors = 0
         try:
-            desired_point = point(direction, coordinate)
-            if desired_point[0] < 0 or desired_point[1] < 0:
+            other_point = point(direction, coordinate)
+            if other_point[0] < 0 or other_point[1] < 0:
                 raise IndexError
-            elif self.state[desired_point] == ALIVE:
-                alive_neighbors += 1
-            elif self.state[desired_point] == DEAD:
+            elif self.state[other_point] == ALIVE:
+                alive_neighbors = 1
+            elif self.state[other_point] == DEAD:
                 pass
+
             else:
                 if self.sub_field:
-                    alive_neighbors += self.sub_field.edge_value(
+                    alive_neighbors = self.sub_field.edge_value(
                         OPPOSITE[direction])
-        except IndexError:
+                elif self.state[coordinate] == ALIVE:
+                    self.sub_field = RecursiveBugField(EMPTY_FIELD,
+                                                       super_field=self)
+                    self.sub_field.set_next_state()
+
+        # Access super field.
+        except IndexError: 
             adjacent_super_coord = COORD_NEXT_TO_MIDDLE[direction]
             if self.super_field:
                 if self.super_field.state[adjacent_super_coord] == ALIVE:
-                    alive_neighbors += 1
+                    alive_neighbors = 1
+            elif self.state[coordinate] == ALIVE:
+                self.super_field = RecursiveBugField(EMPTY_FIELD,
+                                                     sub_field=self)
+                self.super_field.set_next_state()
+                                                        
         return alive_neighbors
 
     def edge_value(self, mode: str):
@@ -183,6 +144,3 @@ class RecursiveBugField(BugField):
             return np.count_nonzero(self.state[:, 0] == ALIVE)
         else:
             raise ValueError(f"Unknown edge mode: {mode}")
-
-    def __repr__(self):
-        return "BF{self.depth}"
